cee6769190e546a95be555024eef5ed3aa1cc41f
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-07-25 09:09:19 -0300

    database settings

--- src/main/java/com/mathvieira/voll/med/Application.java
+++ src/main/java/com/mathvieira/voll/med/Application.java
@@ -3,10 +3,15 @@ package com.mathvieira.voll.med;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(Application.class, args);
 	}
 
